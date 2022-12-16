5e48410dc4ffb86597ababf58b5f748ba1ea06f9
jander.liborio <jandernunes14@gmail.com>
2022-12-15 21:24:40 -0400

    criado loader

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,36 @@
-export function modal() {
-  return 'modal';
+import PropTypes from 'prop-types';
+import ReactDom from 'react-dom';
+
+import { Container, Overlay, Footer } from './styles';
+
+import Button from '../Button';
+
+export function Modal({ danger }) {
+  return ReactDom.createPortal(
+    <Overlay>
+      <Container danger={danger}>
+        <h1>Titulo do modal</h1>
+        <p>Corpo do modal</p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button" danger={danger}>
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>,
+    document.getElementById('modal-root'),
+  );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
